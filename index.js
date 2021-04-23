0d5b237868be1f5c44ba8f1439e28db48345150e
nemostatus <mattcorralejo1@gmail.com>
2021-04-23 13:35:24 -0700

    iterate, set to map,display in div

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ map.set(7,"number") //the second param isnt strict and can be named whatever
 // 1 => number
 // a => b
 
-const groceries =new Map([["product", "price"]])//creates new table with an array wrapping around key value pair names --> ["movie name","genre"]
-groceries.set("chocolate", 2.50)
-console.log(...groceries)
-groceries.set("graham crackers", 2.00)
-groceries.set("marshmallows", 1.75) //set or write a key value pair array in table
-console.log(...groceries)
-console.log(groceries.has("milk")) //has method insert key name and if exists in table true else false
-console.log(groceries.get("graham crackers")) //returns value of key
+// const groceries =new Map([["product", "price"]])//creates new table with an array wrapping around key value pair names --> ["movie name","genre"]
+// groceries.set("chocolate", 2.50)
+// console.log(...groceries)
+// groceries.set("graham crackers", 2.00)
+// groceries.set("marshmallows", 1.75) //set or write a key value pair array in table
+// console.log(...groceries)
+// console.log(groceries.has("milk")) //has method insert key name and if exists in table true else false
+// console.log(groceries.get("graham crackers")) //returns value of key
 
 function tableGui(table){
     console.log("")
@@ -55,25 +55,26 @@ table.forEach((value, key)=>{
     console.log( `---------------------`)
 })} //why use interpolation and template literals without them NaN is returned
 
-tableGui(groceries)
-tableGui(map)
+// tableGui(groceries)
+// tableGui(map)
 
-const chessPieces = new Map([["piece","point"]])
-chessPieces.set("pawn", 1)
-chessPieces.set("knight", 3)
-chessPieces.set("bishop", 3)
-chessPieces.set("rook", 5)
-chessPieces.set("queen", 9)
-chessPieces.set("king", 0)
-tableGui(chessPieces)
+// const chessPieces = new Map([["piece","point"]])
+// chessPieces.set("pawn", 1)
+// chessPieces.set("knight", 3)
+// chessPieces.set("bishop", 3)
+// chessPieces.set("rook", 5)
+// chessPieces.set("queen", 9)
+// chessPieces.set("king", 0)
+// tableGui(chessPieces)
 
-console.log(...chessPieces.keys())
+// console.log(...chessPieces.keys())
 
-const tmnt = new Map([["turtleName","personality"],["rafael","aggressive"],["leonardo","brave"],["donatello","tactical"],["michaelAngelo","chaotic"]])
-tableGui(tmnt)
+// const tmnt = new Map([["turtleName","personality"],["rafael","aggressive"],["leonardo","brave"],["donatello","tactical"],["michaelAngelo","chaotic"]])
+// tableGui(tmnt)
 
 function addFieldToForm(){
     console.log("rhj")
+    //loop through list once submitted and set new instance every other el
     let keytype = document.getElementById("keysDatatype").value
     let valtype = document.getElementById("valueDatatype").value
     
@@ -82,4 +83,14 @@ function addFieldToForm(){
    <li> <label>Key</label><input type="${keytype}"> <label>Value</label><input type="${valtype}">  </li> 
     `
 }
+
+function createTable(event){
+    // let table = document.querySelector("#dynamic > ol")
+    // console.log(table)
+    event.preventDefault()
+    console.log("www.")
+    // document.querySelector("#dynamic > ol").innerHTML =""
+}
+
+//once all fields are filled out i need a way to uniquely set id(maybe ol li # somehow ) long enough to create the table, reset the form and display the table with the key and value
 //user adds data type)dropdown list) and value of key and it passes it to this fn which in turn
