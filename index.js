0d7a3fccdb324241fc5a092befff2b33a51e6686
nemostatus <mattcorralejo1@gmail.com>
2021-04-23 08:34:28 -0700

    iteration example

--- index.js
+++ index.js
@@ -12,6 +12,28 @@ map.set(7,"number")//seems to overwrite the other value ??
 console.log(...map)
 console.log(map.get(7))
 console.log(map.has(80))//is false because it doesnt exist in the table
+map.delete(7)
+console.log(...map)
+map.set(1, 'number')
+map.set('a', 'b')
+
+//iteration
+//obj - for in
+//arr -for of
+//map - for each
+
+map.forEach((value, key) => {
+  console.log(`${key} => ${value}`)
+})
+//first param is the value then key
+//displayed to console 
+// key => value
+// true => boolean
+// false => boolean
+// 1 => number
+// a => b
+
+
 
 
 //array of arrays
