fade302fc204e5bcd3c34a7ed18bd35eeb071b16
nemostatus <mattcorralejo1@gmail.com>
2021-04-23 08:26:36 -0700

    using has method, get method  and get method

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ console.log(...map)
 map.set(true,'boolean')
 map.set(false,'boolean')
 console.log(...map)
+map.set(7,"blah") //the second param isnt strict and can be named whatever
+console.log(...map)
+map.set(7,"number")//seems to overwrite the other value ??
+console.log(...map)
+console.log(map.get(7))
+console.log(map.has(80))//is false because it doesnt exist in the table
+
+
 //array of arrays
 //[[ 'key', 'value' ], [ true, 'boolean' ], [ false, 'boolean' ]]
