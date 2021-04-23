64ddc8ee5d3d466618d58e0d2de68ddafaf24cc7
nemostatus <mattcorralejo1@gmail.com>
2021-04-23 08:44:12 -0700

    practicing creating map tables

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-const emptyMap = new Map()
-const map = new Map([
-  ['key', 'value']
-])
-console.log(...map)
-map.set(true,'boolean')
-map.set(false,'boolean')
-console.log(...map)
-map.set(7,"blah") //the second param isnt strict and can be named whatever
-console.log(...map)
-map.set(7,"number")//seems to overwrite the other value ??
-console.log(...map)
-console.log(map.get(7))
-console.log(map.has(80))//is false because it doesnt exist in the table
-map.delete(7)
-console.log(...map)
-map.set(1, 'number')
-map.set('a', 'b')
+// const emptyMap = new Map()
+// const map = new Map([
+//   ['key', 'value']
+// ])
+// console.log(...map)
+// map.set(true,'boolean')
+// map.set(false,'boolean')
+// console.log(...map)
+// map.set(7,"blah") //the second param isnt strict and can be named whatever
+// console.log(...map)
+// map.set(7,"number")//seems to overwrite the other value ??
+// console.log(...map)
+// console.log(map.get(7))
+// console.log(map.has(80))//is false because it doesnt exist in the table
+// map.delete(7)
+// console.log(...map)
+// map.set(1, 'number')
+// map.set('a', 'b')
 
 //iteration
 //obj - for in
 //arr -for of
 //map - for each
 
-map.forEach((value, key) => {
-  console.log(`${key} => ${value}`)
-})
+// map.forEach((value, key) => {
+//   console.log(`${key} => ${value}`)
+// })
+
+// console.log(map.size, map.entries())
 //first param is the value then key
 //displayed to console 
 // key => value
@@ -33,8 +35,9 @@ map.forEach((value, key) => {
 // 1 => number
 // a => b
 
-
-
-
-//array of arrays
-//[[ 'key', 'value' ], [ true, 'boolean' ], [ false, 'boolean' ]]
+const groceries =new Map([["product", "price"]])
+groceries.set("chocolate", 2.50)
+console.log(...groceries)
+groceries.set("graham crackers", 2.00)
+groceries.set("marshmallows", 1.75)
+console.log(...groceries)
